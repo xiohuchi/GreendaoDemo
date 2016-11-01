2c80b5bd560c43b7f025e10b699b93b05db3e808
yangbin <1250211588@qq.com>
2016-11-01 22:43:26 +0800

    增加布局

--- app/src/main/java/com/app/UserDao.java
+++ app/src/main/java/com/app/UserDao.java
@@ -7,64 +7,68 @@ import greendao.TABUser;
 /**
  * 操作数据库的接口Dao
  *
- * @author  nanchen
- * @date   2016-08-08  17:23:18
- *
+ * @author nanchen
+ * @date 2016-08-08  17:23:18
  */
 public interface UserDao {
 
     /**
      * 插入一个用户
-     * @param user    需要插入的用户对象
-     * @
+     *
+     * @param user
      */
-    void insert(TABUser user) ;
+    void insert(TABUser user);
+
+    /**
+     * 插入多个用户
+     *
+     * @param userList
+     */
+    void insertList(List<TABUser> userList);
 
     /**
      * 获得所有的用户列表
+     *
      * @return 用户列表
      * @
      */
-    List<TABUser> getAllUser() ;
+    List<TABUser> getAllUser();
 
     /**
      * 更新一个用户
+     *
      * @param user 需要更新的用户类
-     * @return      更新后的对象
+     * @return 更新后的对象
      * @
      */
-    TABUser updateUser(TABUser user) ;
+    TABUser updateUser(TABUser user);
 
     /**
      * 根据姓名修改新姓名
-     * @param name1 老名字
-     * @param name2 新名字
+     *
+     * @param nameOld 老名字
+     * @param nameNew 新名字
      * @
      */
-    void updateUser(String name1, String name2) ;
+    void updateUser(String nameOld, String nameNew);
 
     /**
      * 根据id删除用户
+     *
      * @param id 用户主键
      * @
      */
-    void deleteUser(int id) ;
-
-    /**
-     * 异步添加用户
-     * @param user 需要添加的用户对象
-     * @
-     */
-    void insertUserAsync(TABUser user) ;
+    void deleteUser(int id);
 
     /**
      * 按名字或者年龄查找第一个User
      */
-    TABUser findByNameOrAge(String name1, int age1) ;
+    TABUser findByNameOrAge(String name1, int age1);
 
     /**
      * 清楚所有
+     *
      * @
      */
-    void deleteAll() ;
+    void deleteAll();
 }
--- app/src/main/java/com/app/UserDaoImpl.java
+++ app/src/main/java/com/app/UserDaoImpl.java
@@ -16,50 +16,38 @@ import greendao.TABUserDao;
  */
 
 public class UserDaoImpl implements UserDao {
+
+    private greendao.TABUserDao getTABUserDao() {
+        return App.getDaoSession().getTABUserDao();
+    }
+
     @Override
     public void insert(TABUser user) {
-        getTABUserDao().insertOrReplace(user);
-        List<TABUser> userList = new ArrayList<>();
-        getTABUserDao().insertInTx(userList);
-        getTABUserDao().insertOrReplaceInTx(userList);
+
+    }
+
+    @Override
+    public void insertList(List<TABUser> userList) {
+
     }
 
     @Override
     public List<TABUser> getAllUser() {
-        List<TABUser> list;
-        QueryBuilder<TABUser> qb = getTABUserDao().queryBuilder()
-                .where(TABUserDao.Properties.Name.isNotNull(),
-                        TABUserDao.Properties.Name.eq("XiaoMing"),
-                        TABUserDao.Properties.Age.between(20, 30),
-                        TABUserDao.Properties.PhoneNumber.like("177"))
-                .whereOr(TABUserDao.Properties.Age.between(20, 30),
-                        TABUserDao.Properties.PhoneNumber.like("177"))
-                .limit(10)
-                .orderAsc()
-                .orderDesc(TABUserDao.Properties.Name);
-        long count = qb.count();
-        list = qb.list();
-        return getTABUserDao().loadAll();
-    }
-
-    @Override
-    public TABUser updateUser(TABUser user) {
-        getTABUserDao().updateInTx();
         return null;
     }
 
     @Override
-    public void updateUser(String name1, String name2) {
+    public TABUser updateUser(TABUser user) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(String nameOld, String nameNew) {
 
     }
 
     @Override
     public void deleteUser(int id) {
-        getTABUserDao().deleteByKey(1l);
-    }
-
-    @Override
-    public void insertUserAsync(TABUser uselr) {
 
     }
 
@@ -71,9 +59,5 @@ public class UserDaoImpl implements UserDao {
     @Override
     public void deleteAll() {
 
-    }
-
-    private greendao.TABUserDao getTABUserDao() {
-        return App.getDaoSession().getTABUserDao();
     }
 }
